a354653f75ceae4815cb4be844965f7996510f3c
Divyank Rajput <rajputdivyanking@gmail.com>
2026-01-01 02:50:24 +0530

    Initial commit for RecruitFlow Project

---
+++ src/test/java/com/jobportal/recruitflow/RecruitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.recruitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
